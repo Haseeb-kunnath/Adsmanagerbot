ede94325b14dab9a2bf3d19baf4d15ec54490206
Haseeb-kunnath <kunnath.haseebviva@gmail.com>
2024-10-09 13:30:57 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,26 @@
+<?php
+
+define('ADMIN_NAME', 'Ads ADMIN'); // admin name
+define('ADMIN_USERNAME', 'haseebrb'); //admin username without @
+define('ADMIN_CHAT_ID', 1488127132); // admin id
+define('AD_SERVICE_NAME', 'Demo 𝐀𝐃𝐒'); // business name
+define('CHANNEL_USERNAME', 'Adsofficial'); //channel username without @
+define('PAYOUT_GROUP_ID', -100854); //payout group Id or admin id
+define('REGISTER_BOT_USERNAME', 'ads_bot'); //bot username without @
+define('POST_BOT_USERNAME', '@Demo_bot'); //bot username with @
+define('BOT_ID', '2096952203'); // bot id
+
+$dbusr = 'Db user';
+$dbpas = 'Db pass';
+$dbnam = 'Db name';
+
+$token = 'bot Api';
+
+$botcreator = '1990429239';
+$paylink = "https://haseeb.in/adsbot/demo/pay.php"; //bot payment link
+$fname = "adbot"; //folder name
+
+//Go to lib->config_paytm.php and edit details with Paytm APIs on [line 10 & 11].
+//Then edit pgRedirect.php file [line no:6] with your domain.
+// then set webhook 
+// dev = @Haseeb_TG
